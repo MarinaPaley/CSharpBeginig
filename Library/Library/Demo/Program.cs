c9c4d4db710caa82d67abd4161f2706617231c52
Marina Paley <marina_paley@mail.ru>
2022-09-13 18:24:49 +0300

    Начата работа над библиотекой

---
+++ Library/Library/Demo/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Domain;
+var author = new Author("Васильева", "Марина", null);
+var book = new Book("C", author);
+Console.WriteLine(book);
---
+++ Library/Library/Domain/Author.cs
@@ -0,0 +1,60 @@
+﻿/// <summary>
+/// 
+/// </summary>
+namespace Domain
+{
+    /// <summary>
+    /// Автор.
+    /// </summary>
+    public class Author
+    {
+        public Author(string lastName, string firstName, 
+            string? middleName)
+        {
+            if (string.IsNullOrWhiteSpace(firstName))
+            {
+                throw new ArgumentNullException(nameof(firstName));
+            }
+
+            if (string.IsNullOrWhiteSpace(lastName))
+            {
+                throw new ArgumentNullException(nameof(lastName));
+            }
+
+            if ((middleName?.Trim())?.Length == 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(middleName));
+            }
+            this.Id = Guid.NewGuid();
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.MiddleName = middleName;
+        }
+
+        public Guid Id { get; }
+        /// <summary>
+        /// Имя.
+        /// </summary>
+        public string FirstName { get; }
+
+        /// <summary>
+        /// Фамилия.
+        /// </summary>
+        public string LastName { get; }
+
+        /// <summary>
+        /// Отчество.
+        /// </summary>
+        public string? MiddleName { get; }
+
+        /// <summary>
+        /// Книги.
+        /// </summary>
+        public ISet<Book> Books { get; } = new HashSet<Book>();
+
+        public override string ToString()
+        {
+            return $"{LastName} {FirstName} {MiddleName}";
+        }
+    }
+}
